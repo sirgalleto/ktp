3b88cb7d145cd6f1493c3e741a3c9efe1d2509d2
Sebastián Osorio <im@svzosorio.co>
2015-11-24 16:58:57 -0600

    share and addNote methods

--- js/controllers/notes.controller.js
+++ js/controllers/notes.controller.js
@@ -5,13 +5,36 @@
     .module('ktp')
     .controller('NotesController', NotesController);
 
-    function NotesController(Notes) {
+    function NotesController(Notes, $mdBottomSheet, $mdDialog) {
         var vm = this;
 
         construct();
 
+        vm.share = function($event){
+            $mdBottomSheet.show({
+                templateUrl: 'views/share-list.tpl.html',
+                targetEvent: $event
+            });
+        }
+
+        vm.addNote = function($event) {
+            $mdDialog.show({
+                controller: 'AddNoteController',
+                templateUrl: 'views/add-note.tpl.html',
+                parent: angular.element(document.body),
+                targetEvent: $event,
+                clickOutsideToClose: true
+            })
+            .then(function(answer) {
+                construct();
+            }, function() {
+
+            });
+        };
+
         function construct() {
             Notes.get().then(function(data){
+                console.log(data);
                 vm.notes = data;
             });
         }
