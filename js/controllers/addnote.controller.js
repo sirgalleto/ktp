716798f83ee7fecd09582f4e2b3feda5cc5c9a3e
Sebastián Osorio <im@svzosorio.co>
2015-11-24 19:23:02 -0600

    Add note method

--- js/controllers/addnote.controller.js
+++ js/controllers/addnote.controller.js
@@ -5,12 +5,17 @@
     .module('ktp')
     .controller('AddNoteController', AddNoteController);
 
-    function AddNoteController($scope, $mdDialog) {
+    function AddNoteController($scope, $mdDialog, Notes) {
         var vmd = this;
 
         $scope.cancel = function(){
             $mdDialog.cancel();
         };
 
+        $scope.addNote = function(note){
+            Notes.create(note).then(function(){
+                 $mdDialog.hide();
+            });
+        };
     }
 })()
